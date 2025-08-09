0c289f8cc0a61505130d3ce3b1a35b567432ed3c
Dean Agan <agandfr@gmail.com>
2025-08-09 20:59:04 +1000

    Update app.tsx to update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Welcome to GreenThumb!</Text>
+        <Text>Manage your plant inventory and care details</Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-});``
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
